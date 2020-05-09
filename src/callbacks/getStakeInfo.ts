b90ca7c985fc8b75e199b12ef1efe32f9fa111e7
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-09 11:24:48 +0400

    wip: getStakeInfo

--- src/callbacks/getStakeInfo.ts
+++ src/callbacks/getStakeInfo.ts
@@ -1,5 +1,31 @@
+import { checkAuth } from '../initialization/auth';
+import {
+  getMaxStake,
+  getMinStake,
+  getStakeCount,
+  getSumFromStake,
+  checkIsEnabled,
+  getCoefFromCoupon,
+  getParametrFromCoupon,
+} from '../stake-functions';
+import { getBalance } from '../initialization/balance';
 import { log } from '../logger';
 
-export function getStakeInfo() {
-  return '123';
+export function getStakeInfo(): string {
+  worker.StakeInfo.Auth = checkAuth();
+  worker.StakeInfo.StakeCount = getStakeCount();
+  worker.StakeInfo.Balance = getBalance();
+  worker.StakeInfo.MinSumm = getMinStake();
+  worker.StakeInfo.MaxSumm = 500; //getMaxStake();
+  worker.StakeInfo.Summ = getSumFromStake();
+  worker.StakeInfo.IsEnebled = checkIsEnabled();
+  if (worker.StakeInfo.StakeCount === 1) {
+    worker.StakeInfo.Coef = getCoefFromCoupon();
+    worker.StakeInfo.Parametr = getParametrFromCoupon();
+  } else {
+    log('Ошибка в кол-ве открытых купонов!');
+    return;
+  }
+
+  return JSON.stringify(worker.StakeInfo);
 }
