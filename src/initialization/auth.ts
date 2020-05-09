272c2a5c7968339dea5c912f3bc4ff6f1dba54e5
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-09 11:25:32 +0400

    feat: auth() and checkAuth()

--- src/initialization/auth.ts
+++ src/initialization/auth.ts
@@ -1,19 +1,40 @@
 import { log } from '../logger';
-import { fireEvent } from '@kot-shrodingera-team/config/util';
+import {
+  fireEvent,
+  sleep,
+  getElement,
+} from '@kot-shrodingera-team/config/util';
 
-export function auth(login: string, pass: string): boolean {
-  const idEmailForm = document.querySelector(
-    'input#auth_id_email'
-  ) as HTMLInputElement;
+export async function authX(login: string, pass: string): Promise<boolean> {
+  const loginButton = document.querySelector(
+    '#curLoginForm'
+  ) as HTMLButtonElement;
+  loginButton.click();
+  await getElement('.auth', 2000);
+  let loginForm = document.querySelector('#auth_id_email') as HTMLInputElement;
+  const phoneButton = document.querySelector(
+    '.custom-functional-button'
+  ) as HTMLButtonElement;
+
+  if (!worker.Login.includes('@')) {
+    log('Авторизация с телефоном');
+    if (phoneButton) {
+      phoneButton.click();
+    }
+    await getElement('#auth_phone_number', 2000);
+    loginForm = document.querySelector(
+      '#auth_phone_number'
+    ) as HTMLInputElement;
+  }
   const passwordForm = document.querySelector(
-    'input#auth-form-password'
+    '#auth-form-password'
   ) as HTMLInputElement;
   const submitButton = document.querySelector(
     '.auth-button'
   ) as HTMLButtonElement;
-  idEmailForm.value = login;
+  loginForm.value = login;
   passwordForm.value = pass;
-  fireEvent(idEmailForm, 'input');
+  fireEvent(loginForm, 'input');
   fireEvent(passwordForm, 'input');
   if (submitButton) {
     submitButton.click();
