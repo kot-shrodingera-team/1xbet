a414bff9e212dedf18b8e689a4ba61e699131859
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-09 11:25:07 +0400

    feat: functional for setStakeSum

--- src/callbacks/setStakeSum.ts
+++ src/callbacks/setStakeSum.ts
@@ -1,5 +1,13 @@
 import { log } from '../logger';
+import { fireEvent } from '@kot-shrodingera-team/config/util';
 
-export function setStakeSum() {
+export function setStakeSum(sum: number): boolean {
+  const sumInput = document.querySelector('.bet_sum_input') as HTMLInputElement;
+  if (!sumInput) {
+    log('Поле для ввода суммы не найдено');
+    return false;
+  }
+  sumInput.value = sum.toString();
+  fireEvent(sumInput, 'input');
   return true;
 }
