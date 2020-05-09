a88752a506e8200f3f888a476c8e889f2f74a5a8
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-09 11:24:31 +0400

    feat: functional for doStake()

--- src/callbacks/doStake.ts
+++ src/callbacks/doStake.ts
@@ -1,3 +1,14 @@
-export function doStake() {
-  return true;
+import { log } from '../logger';
+export function doStake(): boolean {
+  const button = document.querySelector(
+    'div.footer-controls > div > button'
+  ) as HTMLButtonElement;
+  if (button) {
+    button.click();
+    log('Нажали по кнопке в купоне');
+    return true;
+  } else {
+    log('Кнопка в купоне не найдена');
+    return false;
+  }
 }
