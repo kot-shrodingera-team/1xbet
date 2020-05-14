d8a9c353cb124baed1d23b210d5b909a44c4dfb7
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-14 12:32:26 +0400

    feat: import log, add checkStakeStatus()

--- src/callbacks/checkStakeStatus.ts
+++ src/callbacks/checkStakeStatus.ts
@@ -1,3 +1,12 @@
+import { log } from '../logger';
+
 export function checkStakeStatus() {
-  return true;
+  if (document.querySelector('.c-bet-box__overlay')) {
+    log('Исход заблокирован');
+    return false;
+  }
+  if (document.querySelector('.c-coupon-modal')) {
+    log('Ставка принята');
+    return true;
+  }
 }
