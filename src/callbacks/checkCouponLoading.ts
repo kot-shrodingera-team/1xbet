fa4dba0aed5bd9c19171453ce24c542e0879bb62
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-14 12:32:07 +0400

    feat: checkCouponLoading()

--- src/callbacks/checkCouponLoading.ts
+++ src/callbacks/checkCouponLoading.ts
@@ -1,3 +1,9 @@
-export function checkCouponLoading() {
-  return true;
+import { log } from '../logger';
+
+export function checkCouponLoading(): Boolean {
+  log('Идет загрузка купона');
+  return !Boolean(
+    document.querySelector('.c-coupon-modal') ||
+      document.querySelector('.swal2-popup')
+  );
 }
