dc1e72d0b1c32bef7d0b4d50fe72ffb784338d0b
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-14 12:33:46 +0400

    WIP: add fucntion awaiter

--- src/initialization/showStake.ts
+++ src/initialization/showStake.ts
@@ -1,10 +1,19 @@
 import { log } from '../logger';
 import { checkAuth } from './auth';
 import { updateBalance } from './balance';
+import { getElement } from '@kot-shrodingera-team/config/util';
 
 export async function showStake(): Promise<void> {
   if (typeof worker == 'undefined') {
     log('worker не найден! Перезагружаем страницу');
     location.reload();
   }
+  log('Ожидаем загрузку страницы');
+  await waitLoadPage();
+}
+
+async function waitLoadPage(): Promise<void> {
+  await getElement('#loc_info', 3000);
+  await getElement('.games_content', 5000);
+  await getElement('#cxl-badge', 5000);
 }
