d805be16793f841a55c6a891bdca7d42d96f9dd4
Ruslan Makarov <iwantletitdie@mail.ru>
2020-05-29 10:37:22 +0400

    feat: awaiters

--- src/initialization/showStake.ts
+++ src/initialization/showStake.ts
@@ -2,9 +2,10 @@ import { log } from '../logger';
 import { checkAuth } from './auth';
 import { updateBalance } from './balance';
 import { getElement, sleep } from '@kot-shrodingera-team/config/util';
-import { clearCoupon, getStakeCount } from '../stake-functions';
+import { clearCoupon, getStakeCount, awaiter } from '../stake-functions';
 
 export async function showStake(): Promise<void> {
+  const betId = worker.BetId.split('|');
   if (typeof worker == 'undefined') {
     log('worker не найден! Перезагружаем страницу');
     location.reload();
@@ -15,49 +16,90 @@ export async function showStake(): Promise<void> {
     log('Такой ставки уже не существует!');
     return;
   }
-  await waitLoadPage();
+  await awaiter(() => {
+    return document.getElementById(betId[0]);
+  }, 20000);
 
-  log('Ожидаем загрузку coupon_content');
-  await getElement('.coupon__content', 2000);
-  const buttons = document.querySelectorAll('.bet-title');
+  // if (document.querySelector('c-bet-box')) {
+  //   log('c-bet-box не появился');
+  //   worker.JSFail();
+  //   return;
+  // }
   log('Очищаем купон');
   clearCoupon();
-  await sleep(500);
+
+  await awaiter(() => getStakeCount() === 0, 1000);
+  await getElement('.c-bet-box', 2000);
+  log('Ожидаем загрузку коэфа');
+
   if (getStakeCount() >= 1) {
     log('Ошибка очистки! Есть открытые купоны!');
-    return;
-  }
-
-  for (const button of buttons) {
-    await sleep(100);
-    (button as HTMLButtonElement).click();
-  }
-
-  openCoupon();
-  await getElement('.coupon__content', 1000);
-  if (getStakeCount() === 1) {
-    log('Купон открыт!');
-    worker.JSStop();
-  }
-  return;
-}
-function openCoupon() {
-  const betId = worker.BetId.split('|');
-  const couponButton = document.querySelector(
-    `span[data-type="${betId[3]}"]`
-  ) as HTMLButtonElement;
-  if (couponButton) {
-    log('Пытаемся открыть купон');
-    couponButton.click();
-    return;
-  } else {
-    log('Кнопка открытия купона не найдена, возможно событие закрыто.');
     worker.JSFail();
     return;
   }
-}
 
-async function waitLoadPage(): Promise<void> {
-  await getElement('.bets_content betsscroll', 3000);
-  await getElement('#sports_right', 2000);
+  const currentPeriodBlock = document.getElementById(betId[0]);
+  if (!currentPeriodBlock) {
+    log('Не нашли блок текущего периода ');
+    worker.JSFail();
+    return;
+  }
+  const marketHeaderButtons = [
+    ...currentPeriodBlock.querySelectorAll('.bet-title.min'),
+  ] as HTMLElement[];
+  console.log(marketHeaderButtons);
+  for (const button of marketHeaderButtons) {
+    button.click();
+    await sleep(0);
+  }
+  await sleep(500);
+
+  const betButton = findButton();
+  console.log(betButton);
+  if (!betButton) {
+    log('Ставка не найдена');
+    worker.JSFail();
+    return;
+  }
+  betButton.click();
+  updateBalance();
+  await getElement('.coupon__content', 1000);
+  await getElement('.c-bet-box', 2000);
+  log('Ожидаем загрузку коэфа');
+  await sleep(500);
+  if (getStakeCount() === 1) {
+    log('Купон открыт!');
+    worker.JSStop();
+    return;
+  }
+  log('Ставка не попала в купон');
+  worker.JSFail();
+  return;
+}
+function findButton() {
+  let resultButton = null;
+  const betId = worker.BetId.split('|');
+  const currentPeriodBlock = document.getElementById(betId[0]);
+  if (betId[1] !== 'null') {
+    const couponButtons = currentPeriodBlock.querySelectorAll(
+      `span[data-type="${betId[3]}"]`
+    ) as any;
+    if (couponButtons) {
+      couponButtons.forEach((item: any) => {
+        if (item.textContent.includes(betId[1])) {
+          log('Нашли нужную кнопку');
+          resultButton = item;
+          return;
+        }
+      });
+      return resultButton;
+    }
+    log('Не нашли эту ставку!');
+    return;
+  }
+
+  resultButton = currentPeriodBlock.querySelector(
+    `span[data-type="${betId[3]}"]`
+  ) as any;
+  return resultButton;
 }
