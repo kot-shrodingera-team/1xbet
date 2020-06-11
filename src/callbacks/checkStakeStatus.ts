2b9a0b10b2331cf85c0051483c1c4ac2b64d000d
Ruslan Makarov <iwantletitdie@mail.ru>
2020-06-11 12:02:31 +0400

    fix: checkStakeStatus

--- src/callbacks/checkStakeStatus.ts
+++ src/callbacks/checkStakeStatus.ts
@@ -1,12 +1,18 @@
 import { log } from '../logger';
+import { updateBalance } from '../initialization';
 
 export function checkStakeStatus() {
+  if (document.querySelector('.c-coupon-modal')) {
+    log('Ставка принята');
+    updateBalance();
+    return true;
+  }
+
   if (document.querySelector('.c-bet-box__overlay')) {
     log('Исход заблокирован');
     return false;
   }
-  if (document.querySelector('.c-coupon-modal')) {
-    log('Ставка принята');
-    return true;
-  }
+
+  log('Недокументированный статус ставки');
+  return false;
 }
