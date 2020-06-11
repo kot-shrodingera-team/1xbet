7bdefba05fbdf561cf2afc82923b12bebbdee23f
Ruslan Makarov <iwantletitdie@mail.ru>
2020-06-11 12:02:15 +0400

    fix: checkCouponLoading

--- src/callbacks/checkCouponLoading.ts
+++ src/callbacks/checkCouponLoading.ts
@@ -1,9 +1,20 @@
 import { log } from '../logger';
 
 export function checkCouponLoading(): boolean {
-  log('Идет загрузка купона');
-  return !Boolean(
-    document.querySelector('.c-coupon-modal') ||
-      document.querySelector('.swal2-popup')
-  );
+  const succesModal = document.querySelector('.c-coupon-modal');
+  const popUp = document.querySelector('.swal2-popup');
+  const blockedCoupon = document.querySelector('.c-bet-box__overlay');
+  if (succesModal) {
+    log('Загрузка окончена ставка успешная');
+    return !succesModal;
+  } else if (popUp) {
+    log('Загрузка окончена');
+    return !popUp;
+  } else if (blockedCoupon) {
+    log('Загрузка окончена купон заблокирован');
+    return !blockedCoupon;
+  } else {
+    log('Идет загрузка купона');
+    return true;
+  }
 }
