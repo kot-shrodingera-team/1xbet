1bb01205cbc45a7a08fa339d19263b62bf0fb6ea
Ruslan Makarov <iwantletitdie@mail.ru>
2020-06-25 17:04:59 +0400

    feat: loading count

--- src/callbacks/checkCouponLoading.ts
+++ src/callbacks/checkCouponLoading.ts
@@ -1,19 +1,31 @@
 import { log } from '../logger';
-
+import { getStakeCount } from '../stake-functions';
+import { showStake } from '../initialization';
+let loadingCount = 0;
 export function checkCouponLoading(): boolean {
+  if (getStakeCount() === 0) {
+    log('Купон не открыт! ');
+    showStake();
+    return false;
+  }
+  if (loadingCount > 200) {
+    log('Долгое ожидание загрузки купона');
+    return false;
+  }
   const succesModal = document.querySelector('.c-coupon-modal');
   const popUp = document.querySelector('.swal2-popup');
   const blockedCoupon = document.querySelector('.c-bet-box__overlay');
   if (succesModal) {
     log('Загрузка окончена ставка успешная');
-    return !succesModal;
+    return false;
   } else if (popUp) {
     log('Загрузка окончена');
-    return !popUp;
+    return false;
   } else if (blockedCoupon) {
     log('Загрузка окончена купон заблокирован');
-    return !blockedCoupon;
+    return false;
   } else {
+    loadingCount++;
     log('Идет загрузка купона');
     return true;
   }
