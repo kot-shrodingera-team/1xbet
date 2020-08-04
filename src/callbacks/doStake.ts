9fd9d4661600f3224c91d78c7dc87bdc428ebd9e
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-05 00:06:49 +0600

    Добавлена проверка на коэффициент перед ставкой

--- src/callbacks/doStake.ts
+++ src/callbacks/doStake.ts
@@ -2,6 +2,7 @@ import {
   clearStakeProcessingHungMessageSend,
   clearLoadingCounter,
 } from './checkCouponLoading';
+import getCoefficient from '../stake_info/getCoefficient';
 
 const doStake = (): boolean => {
   const stakeButton = document.querySelector(
@@ -12,6 +13,12 @@ const doStake = (): boolean => {
     worker.Helper.WriteLine(
       'Не найдена кнопка "Сделать ставку". Ставку не сделали'
     );
+    return false;
+  }
+  const actualCoefficient = getCoefficient();
+  worker.Helper.WriteLine(`Коэффициент перед ставкой: ${actualCoefficient}`);
+  if (actualCoefficient < worker.StakeInfo.Coef) {
+    worker.Helper.WriteLine('Коэффициент перед ставкой упал');
     return false;
   }
   stakeButton.click();
