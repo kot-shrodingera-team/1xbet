4037b2e2154e73cd78794bb9db91e63a5837719d
kot-shrodingera <kot.shrodingera@gmail.com>
2020-12-16 14:52:55 +0600

    feat(preCheck): проверка на открытый Live

--- src/show_stake/preCheck.ts
+++ src/show_stake/preCheck.ts
@@ -1,3 +1,19 @@
-const preCheck = async (): Promise<void> => {};
+import { log, getElement } from '@kot-shrodingera-team/germes-utils';
+import JsFailError from './errors/jsFailError';
+import NewUrlError from './errors/newUrlError';
+
+const preCheck = async (): Promise<void> => {
+  if (!/\/live/.test(window.location.pathname)) {
+    log('Открыт не Live', 'crimson');
+    window.location.pathname = '/ru/live/';
+    throw new NewUrlError('Переходим на Live');
+  } else {
+    log('Открыт Live', 'steelblue');
+    const betslip = await getElement('.coupon', 15000);
+    if (!betslip) {
+      throw new JsFailError('Купон не появился');
+    }
+  }
+};
 
 export default preCheck;
