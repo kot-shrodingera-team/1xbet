195367e91507a22fbf54fd165d442b630c9da264
kot-shrodingera <kot.shrodingera@gmail.com>
2021-05-04 21:28:57 +0600

    fix(preCheck): переключение вкладки купона

--- src/show_stake/preCheck.ts
+++ src/show_stake/preCheck.ts
@@ -40,6 +40,30 @@ const preCheck = async (): Promise<void> => {
   log('Есть авторизация', 'steelblue');
   updateBalance();
 
+  const couponTab = await getElement<HTMLElement>(
+    '.c-tabs__header > button:first-child'
+  );
+  if (!couponTab.classList.contains('c-tabs__item--active')) {
+    log('Открыта не вкладка купона ставок', 'crimson');
+    log('Открываем вкладку купона ставок', 'orange');
+    couponTab.click();
+    await Promise.race([
+      getElement('.coupon__no-bets', 5000),
+      getElement('.c-bet-box:not(.c-bet-box--done)', 5000),
+    ]);
+    const emptyCoupon = document.querySelector('.coupon__no-bets');
+    const couponBet = document.querySelector(
+      '.c-bet-box:not(.c-bet-box--done)'
+    );
+    if (emptyCoupon || couponBet) {
+      log('Переключились на вкладку купона ставок', 'steelblue');
+    } else {
+      throw new JsFailError(
+        'Не удалось переключиться на вкладку купона ставок'
+      );
+    }
+  }
+
   const couponCleared = await clearCoupon();
   if (!couponCleared) {
     throw new JsFailError('Не удалось очистить купон');
--- src/stake_info/getStakeCount.ts
+++ src/stake_info/getStakeCount.ts
@@ -1,7 +1,7 @@
 import getStakeCountGenerator from '@kot-shrodingera-team/germes-generators/stake_info/getStakeCount';
 
 const getStakeCount = getStakeCountGenerator({
-  stakeSelector: '.c-bet-box',
+  stakeSelector: '.c-bet-box:not(.c-bet-box--done)',
   // context: () => document,
 });
 
