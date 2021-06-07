c05cc0ebf8fa39c536438e1884473b84628c56e5
kot-shrodingera <kot.shrodingera@gmail.com>
2021-06-07 16:14:18 +0600

    feat(getMinimumStake): имитация минимальной ставки

--- src/stake_info/getMinimumStake.ts
+++ src/stake_info/getMinimumStake.ts
@@ -1,4 +1,17 @@
+import { getWorkerParameter } from '@kot-shrodingera-team/germes-utils';
+
 const getMinimumStake = (): number => {
+  if (
+    getWorkerParameter('fakeMinimumStake') ||
+    getWorkerParameter('fakeAuth') ||
+    getWorkerParameter('fakeOpenStake')
+  ) {
+    const fakeBalance = getWorkerParameter('fakeMinimumStake') as number;
+    if (fakeBalance !== undefined) {
+      return fakeBalance;
+    }
+    return 0;
+  }
   if (worker.Currency === 'RUR' || worker.Currency === 'KZT') {
     return 10;
   }
